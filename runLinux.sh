a24073c7088549b8e39dedb096bfd046a1b3dc5e
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-12-31 20:27:42 -0300

    Update runLinux.sh

--- runLinux.sh
+++ runLinux.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-echo "##################################################"
-echo "+-=-=-=-=-...::: YouDownPy v1.0 :::... -=-=-=-=-=+"
-echo "+ ... Programa para Baixar Vídeos do Youtube ... +"
-echo "##################################################"
-echo "--------------------------------------------------"
-echo "!!! Obs. Seu Vídeo será salvo na pasta do diretório !!!"
+
 
 #comando para rodar arquivo python3
 python3 baixavideo.py
